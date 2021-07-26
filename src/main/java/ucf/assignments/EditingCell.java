052dfda7b6351cae3fca098e6f5599d2b64b01b1
xariadavis <55540782+xariadavis@users.noreply.github.com>
2021-07-26 00:47:46 -0700

    minor design change -- change the color of the textbox on edit

--- src/main/java/ucf/assignments/EditingCell.java
+++ src/main/java/ucf/assignments/EditingCell.java
@@ -28,23 +28,6 @@ class EditingCell extends TableCell<Item, String> {
             createTextField();
             setGraphic(textField);
             textField.selectAll();
-            if(mode){
-                textField.setStyle("""
-                    -fx-font-size: 14;
-                    -fx-background-radius: 15;
-                    -fx-border-radius: 15;
-                    -fx-font-family: 'Segoe UI Light', Regular;
-                    -fx-text-fill: white;
-                    -fx-background-color:  #1f1d2c;""".indent(4));
-            } else {
-                textField.setStyle("""
-                    -fx-font-size: 14;
-                    -fx-background-radius: 15;
-                    -fx-border-radius: 15;
-                    -fx-font-family: 'Segoe UI Light', Regular;
-                    -fx-text-fill: black;
-                    -fx-background-color:  #f6faff;""".indent(4));
-            }
         }
     }
 
@@ -81,6 +64,7 @@ class EditingCell extends TableCell<Item, String> {
 
     private void createTextField() {
         textField = new TextField(getString().replace("$",""));
+        setStyle();
         textField.setMinWidth(175);
         textField.setOnAction((e) -> commitEdit());
         textField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
@@ -94,6 +78,26 @@ class EditingCell extends TableCell<Item, String> {
                 textField.setText(newValue.replaceAll("[^\\d(.)]", ""));
             }
         }));
+    }
+                                   
+    private void setStyle() {
+        if(mode){
+            textField.setStyle("""
+                    -fx-font-size: 14;
+                    -fx-background-radius: 15;
+                    -fx-border-radius: 15;
+                    -fx-font-family: 'Segoe UI Light', Regular;
+                    -fx-text-fill: white;
+                    -fx-background-color:  #1f1d2c;""".indent(4));
+        } else {
+            textField.setStyle("""
+                    -fx-font-size: 14;
+                    -fx-background-radius: 15;
+                    -fx-border-radius: 15;
+                    -fx-font-family: 'Segoe UI Light', Regular;
+                    -fx-text-fill: black;
+                    -fx-background-color:  #f6faff;""".indent(4));
+        }
     }
 
     private String getString() {
